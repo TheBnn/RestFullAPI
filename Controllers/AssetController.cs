e0bf3f821ce58f9e2ded7d14c1c389a33219c346
Konstantin <ds.as.12@mail.ru>
2022-02-08 16:53:31 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AssetController.cs
@@ -0,0 +1,42 @@
+﻿using Entites.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Services.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestFullAPI.Controllers
+{
+    /// <summary>
+    /// Контроллер активов
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AssetsController : Controller
+    {
+        private IAssetRepository _repository;
+
+        public AssetsController(IAssetRepository repository) => _repository = repository;
+
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public async Task<Asset> GetById(int id)
+        {
+            return await _repository.GetById(id);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<Asset> GetByIsinCode([FromQuery(Name = "isin")] string isin)
+        {
+            return await _repository.GetByIsinCode(isin);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IEnumerable<AssetResponse>> Search([FromQuery(Name = "code")] string code, [FromQuery(Name = "activeType")] int? activeType)
+        {
+            return await _repository.Search(code, activeType);
+        }
+    }
+}
---
+++ Controllers/IssuingController.cs
@@ -0,0 +1,38 @@
+﻿using Entites.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Services.Response;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestFullAPI.Controllers
+{
+    /// <summary>
+    /// Контроллер эмитентов
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IssuingCompaniesController : Controller
+    {
+        private IIssuingCompanyRepository _repository;
+
+        public IssuingCompaniesController(IIssuingCompanyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IEnumerable<IssuingCompany>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public async Task<IEnumerable<AssetWithIssuerResponse>> GetAssetsByIssuingId(int id)
+        {
+            return await _repository.GetAssetsByIssuedId(id);
+        }
+    }
+}
